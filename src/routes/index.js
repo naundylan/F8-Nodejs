2bb9bfaec80ca463c3767aabea85fa51f5a42a88
quyen <naundylan@gmail.com>
2025-06-30 21:45:45 +0700

    testing husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 const info = require('./info');
 const site = require('./site');
 
-            function route(app) {
-                // info
-                app.use('/info', info);
-                // app.post('/info', (req, res) => {
-                //     console.log(req.body);
-                //     res.render('info')
-                // })
+        function route(app) {
+            // info
+            app.use('/info', info);
+            // app.post('/info', (req, res) => {
+            //     console.log(req.body);
+            //     res.render('info')
+            // })
 
-                // home search
-                app.use('/', site);
-            }
+            // home search
+            app.use('/', site);
+        }
 
 module.exports = route;
